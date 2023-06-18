7e6b2ae766156409c1a3c0b3598bc63267cf9970
pvasbob <pvasbob@gmail.com>
2023-06-18 16:30:43 -0400

    Simler Better Creation

--- Intro.py
+++ Intro.py
@@ -1,13 +1,32 @@
 import random
 
+
 def rollDice():
-    roll = random.randint(1,100)
-    return roll 
+    roll = random.randint(1, 100)
+    if roll == 100:
+        print(roll, 'roll was 100, you lose .What are the odds ?! Play again!')
+        return False
+    elif roll <= 50:
+        print(roll, 'roll was 1-50, you lose. Play again!')
+        return False
+    elif 100 > roll > 50:
+        print(roll, 'roll was 51-99, you win! *pretty lights flash!')
+        return True
 
 
+def simple_better(funds, initial_wager, wager_count):
+    value = funds
+    wager = initial_wager
 
-x = 0
-while x < 100:
-    result= rollDice()
-    print(result)
-    x += 1
+    currentWager = 0
+    while currentWager < wager_count:
+        if rollDice():
+            value += wager
+        else:
+            value -= wager
+
+        currentWager += 1
+        print(currentWager, 'Funds', value)
+
+
+simple_better(10000, 100, 100)
