6eb6bd5b92727a4a1d6796ab87677b581f10bee7
pvasbob <pvasbob@gmail.com>
2023-06-18 17:23:31 -0400

    No one escape losing money in the long term.

--- Monte_Carlo_Simulation.py
+++ Monte_Carlo_Simulation.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 def rollDice():
@@ -18,22 +20,34 @@ def simple_better(funds, initial_wager, wager_count):
     value = funds
     wager = initial_wager
 
-    currentWager = 0
-    while currentWager < wager_count:
+    wX = []
+    vY = []
+
+    currentWager = 1
+
+    while currentWager <= wager_count:
         if rollDice():
             value += wager
+            wX.append(currentWager)
+            vY.append(value)
         else:
             value -= wager
+            wX.append(currentWager)
+            vY.append(value)
 
         currentWager += 1
 
     if value < 0:
         value = 'broke'
-    print('Funds: ', value)
+    # print('Funds: ', value)
+    plt.plot(wX, vY)
 
 
 x = 0
 
-while x < 100:
-    simple_better(10000, 100, 10000)
+while x < 1000:
+    simple_better(10000, 100, 100000)
     x += 1
+plt.ylabel('Account Value')
+plt.xlabel('Wager Count')
+plt.show()
