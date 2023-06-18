6838f2e349bdfad8e2d847a402db4fa52c2abf02
pvasbob <pvasbob@gmail.com>
2023-06-18 16:47:32 -0400

    Add broke possibility

--- Monte_Carlo_Simulation.py
+++ Monte_Carlo_Simulation.py
@@ -4,13 +4,13 @@ import random
 def rollDice():
     roll = random.randint(1, 100)
     if roll == 100:
-        print(roll, 'roll was 100, you lose .What are the odds ?! Play again!')
+        # print(roll, 'roll was 100, you lose .What are the odds ?! Play again!')
         return False
     elif roll <= 50:
-        print(roll, 'roll was 1-50, you lose. Play again!')
+        # print(roll, 'roll was 1-50, you lose. Play again!')
         return False
     elif 100 > roll > 50:
-        print(roll, 'roll was 51-99, you win! *pretty lights flash!')
+        # print(roll, 'roll was 51-99, you win! *pretty lights flash!')
         return True
 
 
@@ -26,7 +26,14 @@ def simple_better(funds, initial_wager, wager_count):
             value -= wager
 
         currentWager += 1
-        print(currentWager, 'Funds', value)
+
+    if value < 0:
+        value = 'broke'
+    print('Funds: ', value)
 
 
-simple_better(10000, 100, 100)
+x = 0
+
+while x < 100:
+    simple_better(10000, 100, 10000)
+    x += 1
