2d3edf98819fd49338e3ea2c8a82b3bbb7d13bde
pvasbob <pvasbob@gmail.com>
2023-06-18 22:02:12 -0400

    Martingale Strategy

--- Monte_Carlo_Simulation.py
+++ Monte_Carlo_Simulation.py
@@ -1,6 +1,7 @@
-import random
 import matplotlib
+import random
 import matplotlib.pyplot as plt
+import time
 
 
 def rollDice():
@@ -14,6 +15,66 @@ def rollDice():
     elif 100 > roll > 50:
         # print(roll, 'roll was 51-99, you win! *pretty lights flash!')
         return True
+
+
+def doubler_bettor(funds, initial_wager, wager_count):
+    value = funds
+    wager = initial_wager
+
+    wX = []
+    vY = []
+
+    currentWager = 1
+    previousWager = 'win'
+    previousWagerAmount = initial_wager
+
+    while currentWager <= wager_count:
+        if previousWager == 'win':
+            print('we won the last wager, greate')
+            if rollDice():
+                value += wager
+                print(value)
+                wX.append(currentWager)
+                vY.append(value)
+            else:
+                value -= wager
+                previousWager = 'loss'
+                print(value)
+                previousWagerAmount = wager
+                wX.append(currentWager)
+                vY.append(value)
+                if value < 0:
+                    print('we went broke after', currentWager, 'bets')
+                    break
+        elif previousWager == 'loss':
+            print('we lost the laostone, so we will be smart and double')
+            if rollDice():
+                wager = previousWagerAmount * 2
+                print('we win', wager)
+                value += wager
+                print(value)
+                wager = initial_wager
+                previousWager = 'win'
+                wX.append(currentWager)
+                vY.append(value)
+            else:
+                wager = previousWagerAmount * 2
+                print('we lost', wager)
+                value -= wager
+                if value < 0:
+                    print('we went broke after', currentWager, 'bets')
+                    break
+                print(value)
+                previousWager = 'loss'
+
+                previousWagerAmount = wager
+                wX.append(currentWager)
+                vY.append(value)
+
+        currentWager += 1
+
+    print(1, value)
+    plt.plot(wX, vY)
 
 
 def simple_better(funds, initial_wager, wager_count):
@@ -43,11 +104,16 @@ def simple_better(funds, initial_wager, wager_count):
     plt.plot(wX, vY)
 
 
-x = 0
-
-while x < 1000:
-    simple_better(10000, 100, 100000)
-    x += 1
-plt.ylabel('Account Value')
-plt.xlabel('Wager Count')
+doubler_bettor(10000, 100, 1000)
 plt.show()
+time.sleep(555)
+
+
+# x = 0
+
+# while x < 100:
+#     doubler_bettor(10000, 100, 1000)
+#     x += 1
+# plt.ylabel('Account Value')
+# plt.xlabel('Wager Count')
+# plt.show()
